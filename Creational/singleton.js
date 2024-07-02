b004ab3112f40d1820acbd13fe13a5ea5c950dfe
Anup Gupta <anup.gupta@nonstopio.com>
2024-07-02 17:09:04 +0530

    Singleton example 2

--- Creational/singleton.js
+++ Creational/singleton.js
@@ -1,3 +1,5 @@
+// *********** Usage 2 ***********
+
 let instance
 
 class ShoppingBag {
@@ -21,3 +23,26 @@ const bag = Object.freeze(new ShoppingBag()) // Singleton instance
 // export default bag;
 
 const bag2 = new ShoppingBag() // Error: This instance already exists
+
+
+
+
+
+/*
+*********** Usage 2 ***********
+
+let bag = []
+
+const ShoppingBag = {
+    getBag() {
+        console.log(bag)
+    },
+    addItem(item) {
+        bag.push(item)
+    }
+}
+
+const singletonBag = Object.freeze(ShoppingBag) // Singleton instance
+
+export default singletonBag;
+*/
